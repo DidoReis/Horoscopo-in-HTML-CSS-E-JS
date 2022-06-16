9fa77b3a69ef4eb4384f4072768cf9aa1015c656
DidoReis <djdidoreis@gmail.com>
2022-06-16 06:14:53 -0300

    Create color.js

---
+++ Js/color.js
@@ -0,0 +1,14 @@
+let colors = [
+  ["Cor: Vermelha"],
+  ["Cor: Lilás"],
+  ["Cor: Amarelo"],
+  ["Cor: Branco"],
+  ["Cor: Laranja"],
+  ["Cor: Verde"],
+  ["Cor: Rosa"],
+  ["Cor: Vinho"],
+  ["Cor: Violeta"],
+  ["Cor: Preto"],
+  ["Cor: Azul"],
+  ["Cor: Azul Claro"],
+];
